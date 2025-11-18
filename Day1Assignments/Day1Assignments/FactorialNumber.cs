abdc46800be3f9627197b2f736b1a2719ed65fe3
Ashok Kommuru <ashokko@infinite.com>
2025-11-18 22:18:51 +0530

    pushed sucessfully

---
+++ Day1Assignments/Day1Assignments/FactorialNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Assignments
+{
+    internal class FactorialNumber
+    {
+        public static int Factorial()
+        {
+            int num = 5;
+            int fact = 1;
+            for (int i = num; i > 0; i--)
+            {
+                fact *= i;
+            }
+            return fact;
+        }
+        static void Main(string[] args)
+        {
+            Task<int> factorial = Task.Run(() => Factorial());
+            int result = factorial.Result;
+
+            Console.WriteLine("Factorial of the Number is : " + result);
+            Console.ReadLine();
+        }
+    }
+}
+
+ 
+    
+
---
+++ Day1Assignments/Day1Assignments/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Day1Assignments
+{
+    internal class Program
+    {
+        public void method1()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Number :  " + i);
+            }
+        }
+        public void method2()
+        {
+            for (int i = 6; i <= 10; i++)
+            {
+                Console.WriteLine("Number :  " + i);
+            }
+        }
+        public void method3()
+        {
+            Console.WriteLine("All numbers printed!");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Task task1 = new Task(p.method1);
+            task1.Start();
+            Task task2 = new Task(p.method2);
+            task2.Start();
+
+            Task task3 = new Task(p.method3);
+            task3.Start();
+            Task.WaitAll(task1, task2, task3);
+            Console.WriteLine("Done");
+
+            Console.ReadLine();
+
+        }
+    }
+}
