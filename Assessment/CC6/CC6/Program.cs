6d8f0136098bff491ef5b8931c8a7dfd81f0e6c1
Ashok Kommuru <ashokko@infinite.com>
2026-01-07 23:56:30 +0530

    Normalize line endings

---
+++ Assessment/CC6/CC6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CC6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assessment ob = new Assessment();
+            //ob.DisplayAllCourses();
+
+            //Console.Write("enter full name: ");
+            //string fullname = Console.ReadLine();
+            //Console.Write("enter email: ");
+            //string email = Console.ReadLine();
+            //Console.Write("enter department: ");
+            //string department = Console.ReadLine();
+            //Console.Write("enter year of study: ");
+            //int year = Convert.ToInt32(Console.ReadLine());
+            //ob.Addstudent(fullname,email,department,year);
+
+            //Console.Write("enter department: ");
+            //string dept = Console.ReadLine();
+            //ob.SearchbyDept(dept);
+
+            //Console.Write("enter student id: ");
+            //int studentId = Convert.ToInt32(Console.ReadLine());
+            //ob.Enrolledstudent(studentId);
+
+            //ob.Updategrade();
+
+            Disconnected ob1 = new Disconnected();
+            //ob1.Stdcourses();
+            //ob1.Modifycoursecred();
+            //ob1.Insertnewcourses();
+            //ob1.Deletestudent();
+            ob1.GetCoursesBySemester();
+
+            Console.ReadLine();
+
+        }
+    }
+}
