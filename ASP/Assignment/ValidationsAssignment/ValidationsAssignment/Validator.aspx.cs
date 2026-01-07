b7c8b04ddf37871ed390db3b73d1832e8e4e5ae5
Ashok Kommuru <ashokko@infinite.com>
2026-01-07 18:16:45 +0530

    adding

---
+++ ASP/Assignment/ValidationsAssignment/ValidationsAssignment/Validator.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.UI;
+namespace ValidationsAssignment
+{
+    public partial class Validator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+
+        {
+            if (Page.IsValid)
+            {
+                Response.Write("Welcome to profile");
+            }
+            else
+            {
+                Response.Write("Enter correct details before submitting");
+            }
+        }
+    }
+}
+
+
+
+
--- MondayAssignment/MondayAssignment/ForeignKeyAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MondayAssignment
-{
-    internal class ForeignKeyAttribute : Attribute
-    {
-    }
-}
